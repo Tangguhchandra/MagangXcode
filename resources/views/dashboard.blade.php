cef6f5928dcaa4010ba3a43d837363bf66aae5a2
Heksa R <trisnawan987@gmail.com>
2025-07-14 15:00:30 +0700

    Responsive

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -50,6 +50,28 @@
             </button>
         </form>
     </div>
+    <div class="mobile-only">
+    <div class="hamburger" id="hamburger">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+
+  <div class="side-menu" id="sideMenu">
+    <span class="close-btn" id="closeBtn">&times;</span>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#tentang">Tentang</a></li>
+      <li><a href="#program">Program</a></li>
+      <li><a href="#kontak">Kontak</a></li>
+      <li><a href="{{ route('profil') }}">Profil</a></li>
+      <li><a href="{{ route('logout') }}">Logout</a></li>
+    </ul>
+  </div>
+
+  <div class="overlay" id="overlay"></div>
+    </div>
+    </div>
 </nav>
 
     <!-- Hero Section -->
@@ -68,7 +90,7 @@
     <section id="tentang" class="section-light">
         <h2>Tentang Kami</h2>
         <div class="konten-tentang">
-          <img src="{{ asset('images/hero2.HEIC') }}" alt="" width="40%"> 
+          <img src="{{ asset('images/hero2.png') }}" alt="" width="40%"> 
           <p>X-code atau dikenal juga Yogyafree lahir tanggal 5 Juni 2004 di Yogyakarta sebagai media pembelajaran hacking & keamanan komputer yang kemudian di bawah PT. Kode Keamanan Indonesia lalu akhirnya saat ini di bawah PT. Teknologi Server Indonesia.</p>
         </div>
     </section>
@@ -231,6 +253,26 @@
     }
 
     typeWriter();
+
+    const hamburger = document.getElementById('hamburger');
+    const sideMenu = document.getElementById('sideMenu');
+    const overlay = document.getElementById('overlay');
+    const closeBtn = document.getElementById('closeBtn');
+
+    hamburger.addEventListener('click', () => {
+      sideMenu.classList.add('open');
+      overlay.classList.add('show');
+    });
+
+    closeBtn.addEventListener('click', () => {
+      sideMenu.classList.remove('open');
+      overlay.classList.remove('show');
+    });
+
+    overlay.addEventListener('click', () => {
+      sideMenu.classList.remove('open');
+      overlay.classList.remove('show');
+    });
 });
 </script>
 </body>
